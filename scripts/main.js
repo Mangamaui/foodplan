d64f24b77c8ba4e79c11448d1c8c946f1a210d87
Mangamaui <exotic.maui@gmail.com>
2015-01-08 23:23:27 +0100

    cleaned up code

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,4 @@
 $(function() {
 
-    var mainview = new FoodPlanner.MainView();
-
-    var dish = new FoodPlanner.Dish();
-    dish.set('title', 'paprika soep');
-    console.log(typeof(dish.get('title')));
-
+    FoodPlanner.indexView = new FoodPlanner.MainView();
 });
